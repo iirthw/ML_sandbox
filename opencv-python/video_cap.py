b7c15b800ddfd7a9bf5ccb73f91647e92d63d28c
iirthw <iirthw@gmail.com>
2021-10-09 21:59:26 +0300

    show captured frame dimensions

--- opencv-python/video_cap.py
+++ opencv-python/video_cap.py
@@ -3,10 +3,15 @@ import cv2
 
 capture = cv2.VideoCapture(0)
 
+isFirstFrame = True
 while True:
     ret, frame = capture.read()
     width = int(capture.get(3))
     height = int(capture.get(4))
+    if isFirstFrame:
+        isFirstFrame = False
+        print("Video frame width: " + str(width))
+        print("height frame height: " + str(height))
 
     image = np.zeros(frame.shape, np.uint8)
     shrunk_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
