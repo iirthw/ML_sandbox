54515e9b69ac234e823bc5017cbe25b622cfb79d
iirthw <iirthw@gmail.com>
2021-10-12 00:15:06 +0300

    use corner detection on nantes img asset

--- opencv-python/corner.py
+++ opencv-python/corner.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('assets/lenna.png')
+img = cv2.imread('assets/Nantes_lile_1.png')
 img = cv2.resize(img, (0, 0), fx=0.8, fy=0.8)
 img_gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
