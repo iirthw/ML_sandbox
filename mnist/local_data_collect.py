9b8fa5e726b630a8958c3715038c8b8e3a8db7fa
iirthw <iirthw@gmail.com>
2021-11-12 00:09:47 +0200

    [mnist] data_collect: bind q and Escape keys to quit the app

--- mnist/local_data_collect.py
+++ mnist/local_data_collect.py
@@ -1,5 +1,5 @@
+import numpy as np
 from tkinter import *
-
 from PIL import Image, ImageDraw
 
 class DataCollect:
@@ -12,6 +12,8 @@ class DataCollect:
         self.last_x = 0
         self.last_y = 0
         self.canvas = None
+
+        self.app = None
 
         # PIL image twin buffer that mirrors what is drawn on the canvas
         self.image_twin = None
@@ -45,24 +47,28 @@ class DataCollect:
         self.draw_twin.ellipse([top_left_point, bottom_right_point], fill='white')
 
     def on_mouse_right_button(self, event):
-        # filename = 'temp.jpg'
-        # self.imageTwin.save(f\ilename)
-        pixel00 = self.image_twin.getpixel((0, 0))
-        print(pixel00)
+        # In case if a single pixel is needed, use the following: 
+        # pixel00 = self.image_twin.getpixel((0, 0))
+        
+        pixels = list(self.image_twin.getdata())
+        print(pixels)
 
     def clear_canvas(self, event):
         print('clear_canvas')
         self.canvas.delete('all')
+
+    def quit(self, event):
+        self.app.quit()
 
     def run(self):
         black = (0, 0, 0)
         self.image_twin = Image.new('RGB', (self.canvas_width, self.canvas_height), black)
         self.draw_twin = ImageDraw.Draw(self.image_twin)
 
-        app = Tk()
-        app.geometry('200x200')
+        self.app = Tk()
+        self.app.geometry('200x200')
 
-        self.canvas = Canvas(app, bg='black')
+        self.canvas = Canvas(self.app, bg='black')
         self.canvas.pack(anchor='nw', fill='both', expand=1)        
 
         self.canvas.bind('<Button-1>', self.get_xy)
@@ -71,9 +77,12 @@ class DataCollect:
         # that might be confusing if not reading the docs of tkinter.
         self.canvas.bind('<Button-3>', self.on_mouse_right_button)
         self.canvas.bind('<B1-Motion>', self.draw_stroke)
-        app.bind('<space>', self.clear_canvas)
 
-        app.mainloop()
+        self.app.bind('<space>', self.clear_canvas)
+        self.app.bind('<q>', self.quit)
+        self.app.bind('<Escape>', self.quit)
+
+        self.app.mainloop()
  
 dc = DataCollect(stroke_width=16, canvas_width=100, canvas_height=100)
 dc.run()
