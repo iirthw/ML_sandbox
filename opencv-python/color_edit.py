2a97442b183ce5a9baf8a3b55af76376f71c89aa
iirthw <iirthw@gmail.com>
2021-11-19 20:41:10 +0200

    [cv] [color_edit] show rectangle selection area

--- opencv-python/color_edit.py
+++ opencv-python/color_edit.py
@@ -1,6 +1,24 @@
 import cv2
 import random
 
+def shape_selection(event, x, y, flags, param):
+    # See https://stackoverflow.com/questions/60113239/how-to-make-rectangle-visible-while-selecting-a-area-in-image
+    global coordinates
+    if event == cv2.EVENT_LBUTTONDOWN:
+        coordinates = [(x, y)]
+        coordinates.append((x,y))
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if len(coordinates) == 2:
+            coordinates.pop(1)
+            coordinates.append((x,y))
+    elif event == cv2.EVENT_LBUTTONUP:
+        coordinates.pop(1)
+        coordinates.append((x,y))
+    if len(coordinates) == 2:
+        image[:] = image_copy[:]
+        cv2.rectangle(image, coordinates[0], coordinates[1], (0, 0, 255), 1)
+        cv2.imshow("image", image)
+
 imgReadFlag = 1
 img = cv2.imread('assets/lenna.png', imgReadFlag)
 img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5)
