e55e789831e9f8b256016df1ee2c687c7019c4be
iirthw <iirthw@gmail.com>
2021-11-21 21:50:54 +0200

    [cv] move code to main

--- cv2/color_edit.py
+++ cv2/color_edit.py
@@ -1,15 +1,19 @@
 import cv2
 import random
 
-imgReadFlag = 1
-img = cv2.imread('assets/lenna.png', imgReadFlag)
-img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5)
+def main():
+    imgReadFlag = 1
+    img = cv2.imread('assets/lenna.png', imgReadFlag)
+    img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5)
 
-cv2.imshow('lennaImage', img)
+    cv2.imshow('lennaImage', img)
 
-print(type(img))
-print(img.shape)
-print(img[0])
+    print(type(img))
+    print(img.shape)
+    print(img[0])
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
